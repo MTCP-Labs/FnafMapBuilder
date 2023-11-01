8c944e8a856d6f97b01e2f468ca55413b7fa885e
shabman <avia.shabbyman@gmail.com>
2023-11-01 19:45:00 +0000

    menu input handling

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(void) {
 
     std::vector<fnaf::ui::fnaf_menu_title> menu_titles = {
         (fnaf::ui::fnaf_menu_title) { "Welcome to the FNAF 3D Map Builder", 30, 100 },
-        (fnaf::ui::fnaf_menu_title) { "Left Click to begin building", 40, 150 }
+        (fnaf::ui::fnaf_menu_title) { "Left Click to begin building", 25, 30 }
     };
     fnaf::ui::fnaf_menu menu(menu_titles);
 
@@ -14,7 +14,15 @@ int main(void) {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
+        // Menu Handling
         menu.load_menu();
+        menu.handle_input();
+        // End Menu Handling
+        // Build Scene
+        if (!menu.is_menu()) {
+            ClearBackground(BLUE);
+        }
+        // End Build Scene
         EndDrawing();
     }
 
