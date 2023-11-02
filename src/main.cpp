13011498321fcbc37bd9247de0b9ed6387a7b456
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2023-11-02 20:17:55 +0530

    added FPS counter

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ int main(void) {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
+        DrawFPS(1400 - 6 * 12, 0);
         // Menu Handling
         menu.load_menu();
         menu.handle_input();
