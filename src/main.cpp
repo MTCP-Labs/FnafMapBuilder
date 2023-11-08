91cc228e0d901f0bb5de69aa79795626717d415d
Kaushik Kalesh <67593056+Kaushik-Kalesh@users.noreply.github.com>
2023-11-08 06:13:14 +0530

    Changed models directory

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include "log.hpp"
 
 void init_model(Model &model, Texture2D &texture, std::string input) {
-    model = LoadModel(("../raylib/examples/models/resources/models/iqm/" + input + ".iqm").c_str());            // Load the animated model mesh and basic data
-    texture = LoadTexture(("../raylib/examples/models/resources/models/iqm/" + input + ".png").c_str());        // Load model texture and set material
+    model = LoadModel(("models/" + input + ".iqm").c_str());            // Load the animated model mesh and basic data
+    texture = LoadTexture(("models/" + input + ".png").c_str());        // Load model texture and set material
     SetMaterialTexture(&model.materials[0], MATERIAL_MAP_DIFFUSE, texture);                                     // Set model material map texture
 }
 
