65e7e4739d6f73bdd908ba46cb689708017ba9cf
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2023-11-09 21:51:31 +0530

    added InputBox system

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,15 @@
 
 #include "log.hpp"
 #include "modelex.hpp"
+#include "input_box.hpp"
 
 #define MENU_START_X 10
 #define MENU_START_Y 10
+
+float text_size(int fontSize, int len) {
+    const float FONT_SIZE_FACTOR = 0.65;
+    return FONT_SIZE_FACTOR * fontSize * len;
+}
 
 int main(void)
 {
@@ -15,8 +21,7 @@ int main(void)
     const int screenHeight = 1080;
 
     InitWindow(screenWidth, screenHeight, "GAME");
-    SetWindowState(FLAG_WINDOW_MAXIMIZED);
-    DisableCursor();                                 
+    SetWindowState(FLAG_WINDOW_MAXIMIZED);                               
     SetTargetFPS(60);  
 
     // Global Constants
@@ -32,13 +37,14 @@ int main(void)
     camera.projection = CAMERA_PERSPECTIVE;          
 
     // Global variables
-    std::string input;
-
     bool isMenuDisplayed = true;
     bool isModelInitialized = false;
 
-    ModelEx model_man;
+    ModelEx me_man;
     Texture2D texture;
+
+    InputBox ib_pos_y = InputBox(MENU_START_X + text_size(20, 18), MENU_START_Y, text_size(20, 6), 20, "0", RED);       // Initialize input box
+    std::string *input = &ib_pos_y.text;
 
     //--------------------------------------------------------------------------------------
 
@@ -47,7 +53,8 @@ int main(void)
     {
         UpdateCamera(&camera, CAMERA_FREE);
 
-        if (IsKeyPressed(KEY_F2)) {       // Reset the camera
+        // Reset the camera
+        if (IsKeyPressed(KEY_F2)) {       
             camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; 
             camera.target = zeroes;
             camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };     
@@ -59,42 +66,49 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            if (!isMenuDisplayed && IsKeyPressed(KEY_F1)) {     // Display menu
+            // Display menu
+            if (!isMenuDisplayed && IsKeyPressed(KEY_F1)) {     
                 isMenuDisplayed = true;
+                EnableCursor();     // Enable cursor for the menu
             }
 
-            if (isMenuDisplayed && IsKeyPressed(KEY_ENTER)) {      // Hide menu
+            // Hide menu
+            if (isMenuDisplayed && IsKeyPressed(KEY_ENTER)) {   
                 isMenuDisplayed = false;
+                ib_pos_y.isInFocus = false;
 
+                //Initialize model
                 texture = LoadTexture("models/guy.png");               
-                model_man = ModelEx(LoadModel("models/guy.iqm"), (Vector3){0.0f, stof(input), 0.0f}, (Vector3){1.0f, 0.0f, 0.0f}, -90.0f, ones);        // Initialize ModelEx 
-                SetMaterialTexture(&(model_man.model).materials[0], MATERIAL_MAP_DIFFUSE, texture);  
-
+                me_man = ModelEx(LoadModel("models/guy.iqm"), (Vector3){0.0f, stof(*input), 0.0f}, (Vector3){1.0f, 0.0f, 0.0f}, -90.0f, ones);        // Initialize ModelEx 
+                SetMaterialTexture(&(me_man.model).materials[0], MATERIAL_MAP_DIFFUSE, texture);  
                 isModelInitialized = true;
+
+                DisableCursor();        // Disable cursor for the game
             } 
             
+            // Get new y-coordinate of the model as input 
             if(isMenuDisplayed) {
-                int key = GetCharPressed();     // Get new y-coordinate of the model as input 
-                if (key >= (int) '0' && key <= (int) '9' || key == (int) '-') {
-                    input += (char) key;
-                }
-
-                if (IsKeyPressed(KEY_BACKSPACE)) {
-                    input.pop_back();
-                }
-                
                 // Draw menu
                 DrawRectangle(0, 0, screenWidth / 4, screenHeight, Fade(SKYBLUE, 0.5f));
                 DrawRectangleLines(0, 0, screenWidth / 4, screenHeight, BLUE);    
-                DrawText("ENTER MODEL POS_Y: ", MENU_START_X, MENU_START_Y, 20, DARKGREEN);
-                DrawText(input.c_str(), 244, MENU_START_Y, 20, RED);
+                DrawText("ENTER MODEL POS_Y:", MENU_START_X, MENU_START_Y, 20, DARKGREEN);
+
+                // Handle input
+                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) { 
+                    ib_pos_y.in_focus();
+                }
+                if(ib_pos_y.isInFocus) {
+                    ib_pos_y.get_input();
+                }
+                ib_pos_y.draw();
             }
 
             if(!isMenuDisplayed && isModelInitialized) {
+                // Begin 3D scene
                 BeginMode3D(camera);
 
-                    model_man.draw();
-                    DrawGrid(10, 1.0f);
+                    me_man.draw(); 
+                    DrawGrid(1000, 1.0f);
 
                 EndMode3D();
             }
@@ -104,7 +118,7 @@ int main(void)
     }
 
     UnloadTexture(texture);     // Unload texture
-    UnloadModel(model_man.model);       // Unload model  
+    UnloadModel(me_man.model);       // Unload model  
 
     CloseWindow();     
 
