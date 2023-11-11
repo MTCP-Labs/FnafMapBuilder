1e96ec5f64826b676f920a17d0c73f184bf355ef
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2023-11-11 20:38:17 +0530

    Improved InputBox system

--- MapBuilder/include/mapbuilder.hpp
+++ MapBuilder/include/mapbuilder.hpp
@@ -1,18 +1,35 @@
 #ifndef __MAPBUILDER_HPP__
 #define __MAPBUILDER_HPP__
 
+#include <vector>
+#include <cmath>
+
 #include "log.hpp"
 #include "modelex.hpp"
 #include "input_box.hpp"
 
 class MapBuilder {
 private:
-    bool isMenuDisplayed = true;
-    InputBox ib_pos_y;
-    const int MENU_START_X = 10, MENU_START_Y = 10;
+    const int MENU_START_X = 10, MENU_START_Y = 0;
     const Vector3 zeroes = {0.0f, 0.0f, 0.0f}, ones = {1.0f, 1.0f, 1.0f};
+    
+    float *modelex_property_list[3][3];
+
+    InputBox inputBoxList[3][3];
+    InputBox *inFocusInputBox = nullptr;
+
+    bool isMenuDisplayed = true;    
 
     float text_size(int fontSize, int len);
+    
+    void init_modelex_property_list();
+
+    void init_input_box_list();
+    void draw_input_box_list();
+    void set_infocus_input_box();
+    void reset_infocus_input_box();
+
+    void update_modelex();
 public:
     ModelEx modelEx;
     Camera3D camera;
